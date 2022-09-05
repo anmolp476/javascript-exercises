e57a587c17a7fd97aea45fb14bd4748433f89d45
Anmol <anmolp476@gmail.com>
2022-09-04 23:33:18 -0400

    Modified somehow

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) 
+{  
+    let beforeNum = 0;
+    let currentNum = 1;
+    let afterNum = 1;
+    let someArr = [];
+    for(let i = 0; count===num; i++)
+    {
 
+    }
 };
 
 // Do not edit below this line
